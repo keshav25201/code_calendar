f41e8d42f13a1bb56b62e3e0d487e84f4c68f9f0
Saurav jha <jhasaurav215@gmail.com>
2021-03-22 01:06:59 +0530

    organized things

---
+++ Kode_calendar/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KodeCalendarConfig(AppConfig):
+    name = 'Kode_calendar'
---
+++ Kode_calendar/views.py
@@ -0,0 +1,8 @@
+from django.http.response import Http404, HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    context = {}
+    return render(request, "Kode_calendar/index.html", context)
